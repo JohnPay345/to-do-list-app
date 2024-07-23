085bed7aa8a0c6e762ebffe7fd98e2a1fcb1fd88
JohnPay345 <aleshaserg05@mail.ru>
2024-07-23 17:02:23 +0500

    a few changes for a new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './App.module.css'
-import { Input } from './components/input/input'
-import { Button } from './components/button/button'
-import { Container } from './components/container/container'
+import { Input } from './component/input/input'
+import { Button } from './component/button/button'
+import { Task } from './component/task/task'
+import { generateKey } from './features/generateKey'
+
+type taskProps = {
+  name: string;
+  text: string;
+  arrayTasks: Array<React.ReactElement<HTMLDivElement>>;
+}
 
 export const App = () => {
+  const [task, setTask] = useState<taskProps>({
+    name: '',
+    text: '',
+    arrayTasks: []
+  });
+  const [array, setArray] = useState<any>([]);
+
+  const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let newValue = event.target.value;
+    setTask((prev) => (
+      {
+        ...prev,
+        name: newValue
+      }
+    ));
+  }
+
+  const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let newValue = event.target.value;
+    setTask((prev) => (
+      {
+        ...prev,
+        text: newValue
+      }
+    ));
+  }
+
+  const handleClickUnshiftTask = (): undefined => {
+    if (!task.name || !task.text) {
+      return;
+    }
+    let newArrayTasks: React.ReactElement<HTMLDivElement>[] = array;
+    newArrayTasks.unshift(<Task key={generateKey(task.text)} name={task.name} text={task.text} id={generateKey(task.text)} arrayTasks={array} onClick={handleClickDeleteTask}/>);
+    setArray(newArrayTasks);
+    setTask({
+      name: '',
+      text: '',
+      arrayTasks: newArrayTasks
+    });
+  }
+
+  const handleClickDeleteTask = (event: React.MouseEvent<HTMLButtonElement>) => {
+    let newArrayTasks: React.ReactElement<HTMLDivElement>[] = task.arrayTasks;
+    newArrayTasks.shift();
+    setTask((prev) => ({
+      ...prev,
+      arrayTasks: newArrayTasks
+    }))
+  }
+
   return (
-    <header className={style}>
-      <h1>Список дел</h1>
-      <Container>
-        <Container isfdCol={true}>
-          <Input placeholderValue='Название' />
-          <Input placeholderValue='Напишите что-нибудь' />
-        </Container>
-        <Container isfdCol={true} isWidthFlex={true} width='w-3'>
-          <Button>Добавить</Button>
-          <Button>Выделить всё</Button>
-        </Container>
-      </Container>
-    </header>
+    <>
+      <header className={style.header}>
+        <h1>Список дел</h1>
+        <div className={style.container}>
+          <div className={style.containerInputs}>
+            <Input placeholder='Название' value={task.name} onChange={handleChangeName} />
+            <Input placeholder='Напишите что-нибудь' value={task.text} onChange={handleChangeText} />
+          </div>
+          <div className={style.containerButtons}>
+            <Button onClick={handleClickUnshiftTask}>Добавить</Button>
+            <Button>Выделить все</Button>
+          </div>
+        </div>
+        <br />
+        <div className="list-task">
+          {task.arrayTasks ?? null}
+        </div>
+      </header>
+    </>
   )
 }
